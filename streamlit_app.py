35cdae0636999c45a21cfd13f9592174be490ea0
kvohrakv <kvohra000@gmail.com>
2024-06-10 03:44:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,110 +1,51 @@
-import streamlit as st 
-import pandas as pd
+ import streamlit as st
+import speech_recognition as sr
+import pyperclip
+import time 
 
-st.balloons()
-st.markdown("# Data Evaluation App")
+def recognize_speech():
+    # Initialize recognizer class (for recognizing the speech)
+    recognizer = sr.Recognizer()
 
-st.write("We are so glad to see you here. ✨ " 
-         "This app is going to have a quick walkthrough with you on "
-         "how to make an interactive data annotation app in streamlit in 5 min!")
+    # Reading Microphone as source
+    with sr.Microphone() as source:
+        st.write("Speak Anything:")
+        
+        # Adjusting for ambient noise
+        recognizer.adjust_for_ambient_noise(source)
+        
+        # Recording the audio using the microphone
+        audio = recognizer.listen(source)
+        
+        try:
+            # Using Google speech recognition with English language
+            text = recognizer.recognize_google(audio, language="hi-IN")
+            st.session_state["recognized_text"]=text
+        except sr.UnknownValueError:
+            st.error("Sorry, couldn't recognize what you said.")
+        except sr.RequestError as e:
+            st.error("Couldn't request results; {0}".format(e))
 
-st.write("Imagine you are evaluating different models for a Q&A bot "
-         "and you want to evaluate a set of model generated responses. "
-        "You have collected some user data. "
-         "Here is a sample question and response set.")
+def main():
+    st.title("Simple Speech Recognition App")
+    st.write("Click on the 'Start Recording' button and speak into your microphone.")
+    recognize_button = st.button("Start Recording")
+    
+    if recognize_button:
+        recognize_speech()
 
-data = {
-    "Questions": 
-        ["Who invented the internet?"
-        , "What causes the Northern Lights?"
-        , "Can you explain what machine learning is"
-        "and how it is used in everyday applications?"
-        , "How do penguins fly?"
-    ],           
-    "Answers": 
-        ["The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting" 
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds."
-    ]
-}
+    # Get the text to copy when the user clicks on the text area
+    recognized_text = st.session_state.get("recognized_text")
+    if recognized_text:
+        st.text_area("Recognized Text:", recognized_text, key="recognized_text_area")
+        copy_button = st.button("Copy", key="copy_button")
+        if copy_button:
+            # Clearing the clipboard first
 
-df = pd.DataFrame(data)
+            pyperclip.copy(recognized_text)
+            st.write("Text copied to clipboard! You can now paste it elsewhere.")
 
-st.write(df)
+if __name__ == "__main__":
+    main()
 
-st.write("Now I want to evaluate the responses from my model. "
-         "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-         "You will now notice our dataframe is in the editing mode and try to "
-         "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇")
-
-df["Issue"] = [True, True, True, False]
-df['Category'] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config = {
-        "Questions":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Answers":st.column_config.TextColumn(
-            width = "medium",
-            disabled=True
-        ),
-        "Issue":st.column_config.CheckboxColumn(
-            "Mark as annotated?",
-            default = False
-        ),
-        "Category":st.column_config.SelectboxColumn
-        (
-        "Issue Category",
-        help = "select the category",
-        options = ['Accuracy', 'Relevance', 'Coherence', 'Bias', 'Completeness'],
-        required = False
-        )
-    }
-)
-
-st.write("You will notice that we changed our dataframe and added new data. "
-         "Now it is time to visualize what we have annotated!")
-
-st.divider()
-
-st.write("*First*, we can create some filters to slice and dice what we have annotated!")
-
-col1, col2 = st.columns([1,1])
-with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options = new_df.Issue.unique())
-with col2:
-    category_filter = st.selectbox("Choose a category", options  = new_df[new_df["Issue"]==issue_filter].Category.unique())
-
-st.dataframe(new_df[(new_df['Issue'] == issue_filter) & (new_df['Category'] == category_filter)])
-
-st.markdown("")
-st.write("*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`")
-
-issue_cnt = len(new_df[new_df['Issue']==True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
-
-col1, col2 = st.columns([1,1])
-with col1:
-    st.metric("Number of responses",issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
-
-df_plot = new_df[new_df['Category']!=''].Category.value_counts().reset_index()
-
-st.bar_chart(df_plot, x = 'Category', y = 'count')
-
-st.write("Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:")
 
